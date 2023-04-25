01d6e6fd5349b2e7306dab55b1fc419b48327da2
srilakshmi.g <srilakshmi210799@gmail.com>
2023-04-25 09:51:41 +0530

    terraform variabels

---
+++ vars.tf
@@ -0,0 +1,11 @@
+variablr "simple_string"{
+default=hello world
+}
+
+output "simple_string"{
+value=var.simple_string
+}
+
+output "simple_string1"{
+value = "value of simple_string"${var.simple_string}
+}
